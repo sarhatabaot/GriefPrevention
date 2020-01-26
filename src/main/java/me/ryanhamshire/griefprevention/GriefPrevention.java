ac4287c5690c0006ae792b513fa7f9596679b501
Omer Oreg <sarhatabaot1@gmail.com>
2020-01-26 09:03:46 +0200

    Removed depracated method

--- src/main/java/me/ryanhamshire/griefprevention/GriefPrevention.java
+++ src/main/java/me/ryanhamshire/griefprevention/GriefPrevention.java
@@ -355,54 +355,6 @@ public class GriefPrevention extends JavaPlugin {
 		return location.getWorld().getName() + ": x" + location.getBlockX() + ", z" + location.getBlockZ();
 	}
 
-	public boolean abandonClaimHandler(Player player, boolean deleteTopLevelClaim) {
-		PlayerData playerData = this.dataStore.getPlayerData(player.getUniqueId());
-
-		//which claim is being abandoned?
-		Claim claim = this.dataStore.getClaimAt(player.getLocation(), true /*ignore height*/, null);
-		if (claim == null) {
-			GriefPrevention.sendMessage(player, TextMode.Instr, Messages.AbandonClaimMissing);
-		}
-
-		//verify ownership
-		else if (claim.allowEdit(player) != null) {
-			GriefPrevention.sendMessage(player, TextMode.Err, Messages.NotYourClaim);
-		}
-
-		//warn if has children and we're not explicitly deleting a top level claim
-		else if (claim.children.size() > 0 && !deleteTopLevelClaim) {
-			GriefPrevention.sendMessage(player, TextMode.Instr, Messages.DeleteTopLevelClaim);
-			return true;
-		}
-
-		//delete it
-		claim.removeSurfaceFluids(null);
-		this.dataStore.deleteClaim(claim, true, false);
-
-		//if in a creative mode world, restore the claim area
-		if (GriefPrevention.instance.creativeRulesApply(claim.getLesserBoundaryCorner())) {
-			GriefPrevention.addLogEntry(player.getName() + " abandoned a claim @ " + GriefPrevention.getFriendlyLocationString(claim.getLesserBoundaryCorner()));
-			GriefPrevention.sendMessage(player, TextMode.Warn, Messages.UnclaimCleanupWarning);
-			GriefPrevention.instance.restoreClaim(claim, 20L * 60 * 2);
-		}
-
-		//adjust claim blocks when abandoning a top level claim
-		if (Config.config_claims_abandonReturnRatio != 1.0D && claim.parent == null && claim.ownerID.equals(playerData.playerID)) {
-			playerData.setAccruedClaimBlocks(playerData.getAccruedClaimBlocks() - (int) Math.ceil((claim.getArea() * (1 - Config.config_claims_abandonReturnRatio))));
-		}
-
-		//tell the player how many claim blocks he has left
-		int remainingBlocks = playerData.getRemainingClaimBlocks();
-		GriefPrevention.sendMessage(player, TextMode.Success, Messages.AbandonSuccess, String.valueOf(remainingBlocks));
-
-		//revert any current visualization
-		Visualization.revert(player);
-
-		playerData.warnedAboutMajorDeletion = false;
-		return true;
-
-	}
-
 	//helper method to resolve a player by name
 	ConcurrentHashMap<String, UUID> playerNameToIDMap = new ConcurrentHashMap<>();
 
